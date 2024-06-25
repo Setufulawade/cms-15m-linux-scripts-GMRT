cdc54534f621ae4c20ad50b15990cfadca0cf288
SETU FULAWADE <108606163+Setufulawade@users.noreply.github.com>
2024-06-25 13:46:59 +0530

    Update yuminstaller.sh

--- yuminstaller.sh
+++ yuminstaller.sh
@@ -1,43 +1,37 @@
-#version 1.1 date-25-6-2024, Setu Fulwade/JPK $
+#version 1.0 date-21-6-2024, Setu Fulwade/JPK $
 
 #This script will donwload docker and load the docker image for yum package manager.
 
 #!/bin/bash
 
-echo "Docker is not installed. Installing Docker..."
 
-# Update the yum packages
-sudo yum update -y
-if [ $? -ne 0 ]; then
-    echo "Installation failed at yum update."
-    exit 1
-fi
+echo "docker is not installed installing docker"    
 
-# Set up the repository for Docker
-sudo yum install -y yum-utils
-if [ $? -ne 0 ]; then
-    echo "Installation failed at installing yum-utils."
-    exit 1
-fi
+#fucnction to download docker
+download() {
+    #Update the yum packages
+    sudo yum update
 
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-if [ $? -ne 0 ]; then
-    echo "Installation failed at adding Docker repo."
-    exit 1
-fi
+    
+    #Set up the repository for docker
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Install Docker Engine, containerd, and Docker Compose
-sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-if [ $? -ne 0 ]; then
-    echo "Installation failed at installing Docker."
-    exit 1
-fi
 
-echo "Docker installed successfully."
+    #Install Docker Engine, containerd, and Docker Compose:
+    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin    
+}
 
-# Redirect to docker_status.sh
-./runfirefoxdocker.sh
-if [ $? -ne 0 ]; then
-    echo "Failed to execute runfirefoxdocker.sh"
+ #call the download function and store the return value in download_state variable.
+download
+download_status=$?
+
+if [ $download_status -eq 0 ]; 
+then
+    echo "download successful"
+    #redirect to docker_status.sh
+    ./runfirefoxdocker.sh
+else
+    echo "download failed"
     exit 1
 fi
